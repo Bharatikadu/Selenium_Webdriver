166bd86506290d3e925a60b936ca5d132be85b03
Bharatikadu <bharatik030@gmail.com>
2026-07-31 13:23:28 +0530

    Initial commit 31 Juley

---
+++ src/test/java/Webdriver_Method/Ex1_GetClose.java
@@ -0,0 +1,14 @@
+package Webdriver_Method;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Ex1_GetClose
+{
+    public static void main(String[]args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        Thread.sleep(10000);
+        driver.close(); // it closes current or one tab
+    }
+}
---
+++ src/test/java/Webdriver_Method/Ex1_OpenCromeBrowser.java
@@ -0,0 +1,12 @@
+package Webdriver_Method;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Ex1_OpenCromeBrowser
+{
+    public static void main(String[]args)
+    {
+       ChromeDriver driver = new ChromeDriver();
+       driver.close();
+    }
+}
