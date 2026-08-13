71ee5caf4717cfd732e6d2f8e0aec65d12d86217
Bharatikadu <bharatik030@gmail.com>
2026-08-14 00:18:11 +0530

    14August

---
+++ src/test/java/Webelement_Method/Ex5_isSelected1.java
@@ -0,0 +1,29 @@
+package Webelement_Method;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Ex5_isSelected1
+{
+    public  static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        //driver.get("https://www.instagram.com/?hl=en");
+        driver.get("https://testautomationpractice.blogspot.com/");
+        Thread.sleep(2000);
+
+        boolean result= driver.findElement(By.xpath("//input[@id='sunday']")).isSelected();
+        System.out.println(result);
+
+        if(result==true)
+        {
+            System.out.println("Checkbox is selected");
+        }
+        else
+        {
+            System.out.println("Checkbox is not selected");
+        }
+
+    }
+}
