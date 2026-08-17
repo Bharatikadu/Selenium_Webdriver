f578e576b84a6d3504cfd7d88e614dd42e2555e6
Bharatikadu <bharatik030@gmail.com>
2026-08-17 12:42:50 +0530

    17August

---
+++ src/test/java/Webelement_Method/Ex1_sendkeys1.java
@@ -0,0 +1,20 @@
+package Webelement_Method;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Ex1_sendkeys1
+{
+    public static  void main(String args[])
+    {
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com/");
+
+        WebElement s1 = driver.findElement(By.xpath("//textarea[@class='gLFyf']"));
+        s1.sendKeys("abc");
+
+    }
+}
