ce311b7d495b5d6ad739170385242bcc9c7ccf31
Bharatikadu <bharatik030@gmail.com>
2026-08-21 16:47:21 +0530

    21August

---
+++ src/test/java/Practice/Ex2.java
@@ -0,0 +1,14 @@
+package Practice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Ex2
+{
+    public static void main(String[] args)
+    {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+    }
+}
---
+++ src/test/java/s2_Lisrbox/Ex1_SelectOption_SingleSelection.java
@@ -0,0 +1,23 @@
+package s2_Lisrbox;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Ex1_SelectOption_SingleSelection
+{   public static void main(String[] args) {
+
+
+    WebDriver driver = new ChromeDriver();
+    driver.manage().window().maximize();
+    driver.get("https://testautomationpractice.blogspot.com/");
+
+    WebElement Country = driver.findElement(By.xpath("//select[@id='country']"));
+    Select s = new Select(Country);
+    s.selectByVisibleText("United States");
+    s.selectByValue("india");
+    s.selectByIndex(7);
+}
+}
