f2a755c4fe053c69169c6b90bd0ff8251d7d5f67
Bharatikadu <bharatik030@gmail.com>
2026-09-07 01:09:06 +0530

    7sept

--- src/test/java/s2_Popups/Ex3_Switchtomainpage_FromchildWindow.java
+++ src/test/java/s2_Popups/Ex3_Switchtomainpage_FromchildWindow.java
@@ -7,10 +7,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.ArrayList;
 import java.util.Set;
 
-public class Ex3_SwitchtochildWindow
+public class Ex3_Switchtomainpage_FromchildWindow
 {
-    public static  void main(String[] args)
-    {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://skpatro.github.io/demo/links/");
@@ -19,20 +18,22 @@ public class Ex3_SwitchtochildWindow
         driver.findElement(By.xpath("//input[@name='NewTab']")).click();
 
         //get child window id
-        Set<String> allId = driver.getWindowHandles();
-        ArrayList<String> al = new ArrayList<>(allId);
-        String childWindowId= al.get(1);
+        Set<String> allIds = driver.getWindowHandles();
+        ArrayList<String> al = new ArrayList<>(allIds);
 
         //switch to child window
-        driver.switchTo().window(childWindowId);
+        driver.switchTo().window(al.get(1));
 
-        //click on training link from child window
+        //click on training link
         driver.findElement(By.xpath("(//span[text()='Training'])[1]")).click();
+        Thread.sleep(2000);
 
+        //switch to main page
+        driver.switchTo().window(al.get(0));
+        Thread.sleep(2000);
 
-
-
-
+        //click on newWindow button from main page
+        driver.findElement(By.xpath("//input[@name='NewWindow']")).click();
 
 
     }
